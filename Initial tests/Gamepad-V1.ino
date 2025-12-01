c77cff16753b8bbcc724246f63a30ea52906872c
Xiuvas <victorignr@gmail.com>
2025-12-01 01:10:17 -0300

    Create Gamepad-V1.ino

---
+++ Initial tests/Gamepad-V1.ino
@@ -0,0 +1,58 @@
+#include <Joystick.h>
+
+Joystick_ Joystick(
+  0x04,
+  JOYSTICK_TYPE_GAMEPAD,
+  6, 0,
+  true, true,
+  true, false,
+  false, false,
+  false, false,
+  false, false, false
+);
+
+const int pitchPin = A1;
+const int rollPin = A0;
+const int botao1 = 2;
+const int throttlePin = A2;
+// int throttleValue = 1;
+// int lastThrottleValue = -1;
+
+void setup() {
+
+  pinMode(botao1, INPUT_PULLUP);
+  pinMode(pitchPin, INPUT);
+  pinMode(rollPin, INPUT);
+  // Joystick.setZAxisRange(0, 1023);
+  Joystick.begin();
+  delay(1000);
+}
+
+void loop() {
+  int pitchRaw = analogRead(pitchPin);
+  int rollRaw = analogRead(rollPin);
+  
+  // Eliminar ruído mínimo (3-1020)
+  pitchRaw = constrain(pitchRaw, 3, 1020);
+  rollRaw = constrain(rollRaw, 3, 1020);
+  
+  //throttle
+  // throttleValue = analogRead(throttlePin);
+
+ // if(throttleValue != lastThrottleValue){
+   // Joystick.setZAxis(throttleValue);
+    //lastThrottleValue = throttleValue;
+  //}
+ // Serial.println(throttleValue);
+
+  // Enviar valores diretamente - sem map desnecessário
+  Joystick.setYAxis(map(pitchRaw, 3, 1020, 0, 65535));
+  Joystick.setXAxis(map(rollRaw, 3, 1020, 0, 65535));
+  Joystick.setButton(0, digitalRead(botao1) == LOW);
+
+  if(digitalRead(botao1) == LOW) {
+    Serial.println("botao apertado");
+  }
+
+  delay(10);
+}
